7872c1b2ec5e731971aa0b7beee068bd89b357a2
Songpol Anannetikul <songpol.anannetikul@gmail.com>
2022-08-24 11:54:03 +0700

    Add LIFF init

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import liff from "@line/liff"
 import { Routes, Route } from "@solidjs/router"
 
 import Home from "./Home"
 import Register from "./Register"
 
-export default () => (
-  <Routes>
-    <Route path="/" component={Home} />
-    <Route path="/register" component={Register} />
-  </Routes>
-)
+export default async () => {
+  await liff.init({
+    liffId: process.env.LIFF_ID, // Use own liffId
+  })
+  return (
+    <Routes>
+      <Route path="/" component={Home} />
+      <Route path="/register" component={Register} />
+    </Routes>
+  )
+}
